e13f55c01ea1497896b44074e97b31af28a2ecd1
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2023-03-20 14:00:33 +0200

    Assignment has been done

--- Problem1.pl
+++ Problem1.pl
@@ -1,7 +1,4 @@
-is_friend(X, Y) :-
-    friend(X, Y).
-is_friend(X, Y) :-
-    friend(Y, X).
+
 
 friend(ahmed, samy).
 friend(ahmed, fouad).
@@ -31,9 +28,104 @@ friend(zainab, rokaya).
 friend(zainab, eman).
 friend(eman, laila).
 
-friendList(X, L) :-
-    findall(Y, friend(X, Y), L).
+#Task 1:
+is_friend(X, Y) :-
+    friend(X, Y).
+is_friend(X, Y) :-
+    friend(Y, X).
 
+
+# Task 2:
+friendList(Person, []) :- \+ friend(Person, _).
+friendList(Person, [Friend|Rest]) :-
+    friend(Person, Friend),
+    friendList(Person, Rest).
+
+
+# Task 3:
+friendListCount(Person, Count) :- friendListCount(Person, 0, Count).
+
+friendListCount(Person, Acc, Count) :-
+    friend(Person, Friend),
+    NewAcc is Acc + 1,
+    friendListCount(Friend, NewAcc, Count).
+    
+friendListCount(_, Count, Count).
+
+#Task 4:
+
+peopleYouMayKnow(Person, SuggestedFriends) :-
+    findall(Friend, suggestFriend(Person, Friend), SuggestedFriends).
+
+suggestFriend(Person, Friend) :-
+    friend(Person, X),
+    friend(Friend, X),
+    not(friend(Person, Friend)),
+    Person \= Friend.
+
+peopleYouMayKnow(Person, [SuggestedFriend|Rest]) :-
+    suggestFriend(Person, SuggestedFriend),
+    not(member(SuggestedFriend, Rest)),
+    peopleYouMayKnow(Person, Rest).
+peopleYouMayKnow(_, []).
+
+
+#Task 5
+peopleYouMayKnow(Person, N, SuggestedFriend) :-
+    friendList(Person, Friends),
+    findSuggestedFriend(Person, N, Friends, SuggestedFriend).
+
+findSuggestedFriend(_, _, [], _) :-
+    fail.
+findSuggestedFriend(Person, N, [Friend|Rest], SuggestedFriend) :-
+    friendListCount(Friend, Count),
+    Count >= N,
+    \+ friend(Friend, Person),
+    \+ member(Friend, Rest),
+    SuggestedFriend = Friend.
+findSuggestedFriend(Person, N, [_|Rest], SuggestedFriend) :-
+    findSuggestedFriend(Person, N, Rest, SuggestedFriend).
+
+# Task 6
+
+peopleYouMayKnowList(Person, List) :-
+    findPossibleFriends(Person, [], Friends),
+    removeDuplicates(Friends, [], List).
+
+findPossibleFriends(Person, Acc, List) :-
+    friend(Person, Friend),
+    \+ member(Friend, Acc),
+    \+ friend(Person, Friend), % to avoid suggestions of existing friends
+    findPossibleFriends(Person, [Friend|Acc], List).
+
+findPossibleFriends(_, List, List).
+
+removeDuplicates([], Acc, Acc).
+removeDuplicates([H|T], Acc, List) :-
+    member(H, Acc),
+    removeDuplicates(T, Acc, List).
+removeDuplicates([H|T], Acc, List) :-
+    \+ member(H, Acc),
+    removeDuplicates(T, [H|Acc], List).
+
+# Task7
+peopleYouMayKnow_indirect(Person, Friend) :-
+    friendOf(Person, F1),
+    friendOf(F1, F2),
+    \+ friendOf(Person, F2),
+    \+ Person = F2,
+    friendOf(F2, Friend),
+    \+ friendOf(Person, Friend),
+    \+ Person = Friend.
+    
+peopleYouMayKnow_indirect(Person, Friend) :-
+    friendOf(Person, F1),
+    friendOf(F1, F2),
+    \+ friendOf(Person, F2),
+    \+ Person = F2,
+    peopleYouMayKnow_indirect(F2, Friend),
+    \+ friendOf(Person, Friend),
+    \+ Person = Friend.
 
 
 
