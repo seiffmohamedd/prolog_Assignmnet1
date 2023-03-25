4cf0ae62ca1a1a235966927bd4199e269517130b
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2023-03-25 10:30:03 +0200

    tandeef

--- Problem1.pl
+++ Problem1.pl
@@ -36,22 +36,7 @@ is_friend(X, Y) :-
 
 
 % Task 2:
-friendList(X, L) :-
-    findall(Y, friend(X, Y), L).
 
-%Task 3:
-friendListCount([],0).
-friendListCount([_|T], N):-
-    friendListCount(T,NN),
-    N is NN+1.
-%this task is done when we can pass arguments as name not list
-%
-% Task 3 part 2:
-% friendListCount([],0).
-%
-% friendListCount([_|T], N):-
-% friendListCount(T,NN),
-%  N is NN+1.
 
 
 %Task 4
@@ -59,64 +44,4 @@ friendListCount([_|T], N):-
 peopleYouMayKnow(X, Y):-
     is_friend(Z, X),
     is_friend(Z, Y),X\==Y.
-
-
-%Task 5
-peopleYouMayKnow(Person, N, SuggestedFriend) :-
-    friendList(Person, Friends),
-    findSuggestedFriend(Person, N, Friends, SuggestedFriend).
-
-findSuggestedFriend(_, _, [], _) :-
-    fail.
-findSuggestedFriend(Person, N, [Friend|Rest], SuggestedFriend) :-
-    friendListCount(Friend, Count),
-    Count >= N,
-    \+ friend(Friend, Person),
-    \+ member(Friend, Rest),
-    SuggestedFriend = Friend.
-findSuggestedFriend(Person, N, [_|Rest], SuggestedFriend) :-
-    findSuggestedFriend(Person, N, Rest, SuggestedFriend).
-
-%Task 6
-
-peopleYouMayKnowList(Person, List) :-
-    findPossibleFriends(Person, [], Friends),
-    removeDuplicates(Friends, [], List).
-
-findPossibleFriends(Person, Acc, List) :-
-    friend(Person, Friend),
-    \+ member(Friend, Acc),
-    \+ friend(Person, Friend), % to avoid suggestions of existing friends
-    findPossibleFriends(Person, [Friend|Acc], List).
-
-findPossibleFriends(_, List, List).
-
-removeDuplicates([], Acc, Acc).
-removeDuplicates([H|T], Acc, List) :-
-    member(H, Acc),
-    removeDuplicates(T, Acc, List).
-removeDuplicates([H|T], Acc, List) :-
-    \+ member(H, Acc),
-    removeDuplicates(T, [H|Acc], List).
-
-% Task7
-peopleYouMayKnow_indirect(Person, Friend) :-
-    friendOf(Person, F1),
-    friendOf(F1, F2),
-    \+ friendOf(Person, F2),
-    \+ Person = F2,
-    friendOf(F2, Friend),
-    \+ friendOf(Person, Friend),
-    \+ Person = Friend.
-
-peopleYouMayKnow_indirect(Person, Friend) :-
-    friendOf(Person, F1),
-    friendOf(F1, F2),
-    \+ friendOf(Person, F2),
-    \+ Person = F2,
-    peopleYouMayKnow_indirect(F2, Friend),
-    \+ friendOf(Person, Friend),
-    \+ Person = Friend.
-
-
 
